30a74e316f1d528021914a543591240f5b4d1ca1
Valentin Levin <levin_valentin@mail.ru>
2024-10-09 19:54:57 +0500

    work 2024.10.09

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    _min;
+    _max;
+    _last_answer;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._last_answer = this._min + Math.floor((this._max - this._min) / 2);
+        return this._last_answer;
     }
 
     lower() {
-
+        this._max = this._last_answer;
     }
 
     greater() {
-
+        this._min = this._last_answer;
     }
 }
 
